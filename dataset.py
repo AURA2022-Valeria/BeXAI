0715c8922cce99286d7ae80dd04bfdd75814010d
Kidus <se.kidus.yoseph@gmail.com>
2022-06-14 14:44:52 -0400

    implement text classification explanations

--- dataset.py
+++ dataset.py
@@ -1,15 +1,23 @@
+import json
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.neural_network import MLPClassifier
 from sklearn.tree import DecisionTreeClassifier
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.pipeline import make_pipeline
+
 import xgboost as xgb
+
 import lime
 from lime import lime_tabular
+from lime.lime_text import LimeTextExplainer
+import shap
+
 import pickle
 import os
-import shap
 import time
+
 
 
 #class Dataset(): #abstract class
@@ -17,43 +25,113 @@ import time
 # class RegressionDataset(Dataset):
 
 
-class Classification_Dataset:
-    #TODO : divide the class to regreesion and classification datasets 
-    #TODO : the test_size can be passed as a parameter. should it?
-    
-    def __init__(self,dataset_name,X,Y,type,feature_names,class_names,categorical_features=[]):
+class Classification:
+    def __init__(self,dataset_name,X,Y,class_names):
         """
         dataset_name : unique name to the dataset
         X : features of the dataset
         Y : column to be predicted
-        type: Type of the dataset - Tabular,Text or image
         class_names : name of classes on classification dataset
         feature_names : Name of columns in the dataset used to train models
         categorical features : 
         """
         self.dataset_name = dataset_name
-        self.X_train,self.X_test,self.Y_train,self.Y_test = train_test_split(X, Y,test_size=0.2, random_state=0)        
-        self.type = type
-        self.feature_names = feature_names
+        self.X_train,self.X_test,self.Y_train,self.Y_test = train_test_split(X, Y,test_size=0.3, random_state=0)
         self.class_names = class_names
-        self.categorical_features = categorical_features
         self.models = {}
-        self.load_models() #dictionary of trained models with their label
+        self.ml_algorithms = {"random_foest" : RandomForestClassifier(), 
+                        "knn" : KNeighborsClassifier() , 
+                        # "MLP" : MLPClassifier(hidden_layer_sizes=(120, 120,120), activation='relu', solver='lbfgs', max_iter=10000,alpha=1e-5,tol=1e-5), 
+                        "decision_tree" : DecisionTreeClassifier(), 
+                        "xgboost" : xgb.XGBClassifier(n_estimators=100, learning_rate=0.1, gamma=0, subsample=0.5,colsample_bytree=1, max_depth=8)
+                        }
+
+
+
+class TextClassification(Classification):
+    def __init__(self,dataset_name,X,Y,class_names):
+        super().__init__(dataset_name,X,Y,class_names)
+        self.tfidf = TfidfVectorizer()
+        self.X_train_vectorized = self.tfidf.fit_transform(self.X_train)
+        self.X_test_vectorized  = self.tfidf.transform(self.X_test)
+        self.load_models()
+
+    def load_models(self):
+        file_path = f'models/{self.dataset_name}'
+
+        for label,ml_algorithm in self.ml_algorithms.items():
+            if not os.path.exists(file_path):
+                os.makedirs(file_path)
+
+            model_file_path = f'{file_path}/{label}' 
+            try:
+                self.models[label] = pickle.load(open(model_file_path, "rb")) 
+            except:
+                self.models[label] = ml_algorithm.fit(self.X_train_vectorized,self.Y_train)
+                pickle.dump(self.models[label], open(model_file_path, "wb")) #save the trained model inorder not to train it again
+                
+    def get_explanations(self):
+        """
+        Explains blackbox models and measure the time taken for the explainers
+        """
+        #TODO: modularize the time measurement 
+        #TODO: Should graph representation be included in time measurements?
+        index_to_explain = 10
+        timer = {
+            "lime" : {},
+            "shap" : {},
+            "anchor" : {},
+        }
+        
+        row_to_explain = self.X_train[index_to_explain]     
+        #lime explanation
+        explainer = LimeTextExplainer(class_names=self.class_names)
+        for label,model in self.models.items():
+            start = time.time()
+            pipeline = make_pipeline(self.tfidf,model)
+            exp = explainer.explain_instance(row_to_explain, pipeline.predict_proba, num_features=6,top_labels = 3)
+            feature_weight = exp.as_map() #weigths given by lime for words
+            end = time.time()
+            timer["lime"][f'{self.dataset_name} - {label}'] = end - start
+
+            path = f'Explanations/{self.dataset_name}'
+            if not os.path.exists(path):
+                os.makedirs(path)
+            exp.save_to_file(f'{path}/lime_{label}.html')
+        
+        
+        #save the time it took to explain in a json file
+        with open(f"Running_Time/explanation_time_{self.dataset_name}.json", "w") as outfile:
+            json.dump(timer, outfile)
+
+
+
+class TabularClassification(Classification):
+    #TODO : divide the class to regreesion and classification datasets 
+    #TODO : the test_size can be passed as a parameter. should it?
+    
+    def __init__(self,dataset_name,X,Y,feature_names,class_names,categorical_features=[]):
+        super().__init__(dataset_name,X,Y,class_names)
+        self.feature_names = feature_names
+        self.categorical_features = categorical_features
+        self.load_models()
     
     def load_models(self):
-        ml_algorithms = {"random_foest" : RandomForestClassifier(), 
-                        "knn" : KNeighborsClassifier() , 
-                        "MLP" : MLPClassifier(hidden_layer_sizes=(120, 120,120, 120), activation='relu', solver='lbfgs', max_iter=10000,alpha=1e-5,tol=1e-5), 
-                        "decision_tree" : DecisionTreeClassifier(), 
-                        "xgboost" : xgb.XGBClassifier(n_estimators=100, learning_rate=0.1, gamma=0, subsample=0.5,colsample_bytree=1, max_depth=8)}
+        """
+        Loads trained models if they are saved otherwise trains new models
+        """
         file_path = f'Models/{self.dataset_name}'
 
-        for label,ml_algorithm in ml_algorithms.items():          
+        for label,ml_algorithm in self.ml_algorithms.items():
+            if not os.path.exists(file_path):
+                os.makedirs(file_path)
+
+            model_file_path = f'{file_path}/{label}' 
             try:
-                self.models[label] = pickle.load(open(file_path, "rb"))
+                self.models[label] = pickle.load(open(model_file_path, "rb")) 
             except:
                 self.models[label] = ml_algorithm.fit(self.X_train,self.Y_train)
-                pickle.dump(self.models[label], open(file_path, "wb"))
+                pickle.dump(self.models[label], open(model_file_path, "wb")) #save the trained model inorder not to train it again
 
         # for label,model in self.models.items():
         #     try:
@@ -63,6 +141,9 @@ class Classification_Dataset:
 
         
     def get_explanations(self):
+        """
+        Explains blackbox models and measure the time taken for the explainers
+        """
         #TODO: modularize the time measurement 
         #TODO: Should graph representation be included in time measurements?
         index_to_explain = 10
@@ -72,40 +153,41 @@ class Classification_Dataset:
             "anchor" : {},
         }
 
-        if self.type == "Tabular":
-            row_to_explain = self.X_test.iloc[index_to_explain]
-           
-            #lime explanation
-            lime_explainer = lime_tabular.LimeTabularExplainer(self.X_train.values,class_names=self.class_names,feature_names=self.feature_names,discretize_continuous=True)
-            for label,model in self.models.items():
-                start = time.time()
-                exp = lime_explainer.explain_instance(row_to_explain.values, model.predict_proba,top_labels=1)
-                feature_weight = exp.as_map()
-                end = time.time()
-                timer["lime"][f'{self.dataset_name} - {label}'] = end - start
-
-                path = f'Explanations/{self.dataset_name}'
-                if not os.path.exists(path):
-                    os.makedirs(path)
-                exp.save_to_file(f'{path}/lime_{label}.html')
+        row_to_explain = self.X_test.iloc[index_to_explain]
         
+        #lime explanation
+        lime_explainer = lime_tabular.LimeTabularExplainer(self.X_train.values,class_names=self.class_names,feature_names=self.feature_names,discretize_continuous=True)
+        for label,model in self.models.items():
+            start = time.time()
+            exp = lime_explainer.explain_instance(row_to_explain.values, model.predict_proba,top_labels=1)
+            feature_weight = exp.as_map() #weigths given by lime for features
+            end = time.time()
+            timer["lime"][f'{self.dataset_name} - {label}'] = end - start
 
-            #shap explanation
-            k_explainer = shap.KernelExplainer(model.predict_proba, self.X_train)
-            for label,model in self.models.items():
-                start = time.time()
-                shap_values = k_explainer.shap_values(row_to_explain)
-                end = time.time()
-                timer["shap"][f'{self.dataset_name} - {label}'] = end - start
+            path = f'Explanations/{self.dataset_name}'
+            if not os.path.exists(path):
+                os.makedirs(path)
+            exp.save_to_file(f'{path}/lime_{label}.html')
+    
 
-                path = f'Explanations/{self.dataset_name}'
-                if not os.path.exists(path):
-                    os.makedirs(path)
-                shap.initjs()
-                figure = shap.force_plot(k_explainer.expected_value[1], shap_values[1], row_to_explain,matplotlib = True, show = False)
-                figure.savefig(f'{path}/shap_{label}.png')
+        #shap explanation
+        k_explainer = shap.KernelExplainer(model.predict_proba, self.X_train)
+        for label,model in self.models.items():
+            start = time.time()
+            shap_values = k_explainer.shap_values(row_to_explain)
+            end = time.time()
+            timer["shap"][f'{self.dataset_name} - {label}'] = end - start
 
-        return timer
+            path = f'Explanations/{self.dataset_name}'
+            if not os.path.exists(path):
+                os.makedirs(path)
+            shap.initjs()
+            figure = shap.force_plot(k_explainer.expected_value[1], shap_values[1], row_to_explain,matplotlib = True, show = False)
+            figure.savefig(f'{path}/shap_{label}.png')
+
+        #save the time it took to explain in a json file
+        with open(f"Running_Time/explanation_time_{self.dataset_name}.json", "w") as outfile:
+            json.dump(timer, outfile)
             
             
             
--- main.py
+++ main.py
@@ -1,20 +1,57 @@
 from sklearn.datasets import load_wine
 import pandas as pd
-from dataset import Classification_Dataset
+from dataset import TabularClassification,TextClassification
 
-#wine_dataset
-wine_data = load_wine()
-df = pd.DataFrame(wine_data.data, columns=wine_data.feature_names)
-wine_dataset = Classification_Dataset(dataset_name="wine",X=df,Y=wine_data.target,type="Tabular",feature_names=wine_data.feature_names,class_names=list(wine_data.target_names))
+
+##titanic_dataset
+data = pd.read_csv("./datasets/titanic.csv")
+
+#data clean up
+data['Deck'] = data['Cabin'].str.extract(r'([A-Z])?(\d)')[0]
+data['Age'] = data['Age'].fillna(data['Age'].mean())
+data['Deck'] = data['Deck'].fillna("0")
+
+data['Embarked'] = data['Embarked'].fillna('S') #use the most frequency pick up place
+data['Embarked'] = pd.Categorical(data['Embarked'], categories=['S', 'C', 'Q'])
+df = pd.get_dummies(data, columns=["Pclass","Sex","Parch","Deck","Embarked"], prefix=["Pclass","Sex","Parch","Deck","Embarked"])
+
+#drop unused columns
+to_remove = ["Name","PassengerId","Ticket","Cabin"]
+for col in to_remove:
+  df.drop(col,axis=1,inplace=True)
+
+features = list(df.columns) 
+class_names = ["No","Yes"]
+features.remove('Survived')
+
+titanic_dataset = TabularClassification(dataset_name="Titanic",X=df[features],Y=df.Survived,feature_names=features,class_names=class_names,categorical_features=[])
+titanic_dataset.get_explanations()
+
+
+
+# #wine_dataset
+# wine_data = load_wine()
+# df = pd.DataFrame(wine_data.data, columns=wine_data.feature_names)
+# wine_dataset = TabularClassification(dataset_name="wine",X=df,Y=wine_data.target,feature_names=wine_data.feature_names,class_names=list(wine_data.target_names))
 # wine_dataset.get_explanations()
 
-#iris_flower
-data = pd.read_csv("./datasets/iris.csv")
-iris_class_names = ["Setosa","Versicolor","Virginica"]
-iris_features = list(data.columns)
-prediction_feature = "variety"
-iris_features.remove(prediction_feature)
-iris_dataset = Classification_Dataset(dataset_name="iris",X=data[iris_features],Y=data[prediction_feature],type="Tabular",feature_names=iris_features,class_names=iris_class_names)
-iris_timer = iris_dataset.get_explanations()
 
-print(iris_timer)
+# #iris_flower
+# data = pd.read_csv("./datasets/iris.csv")
+# iris_class_names = ["Setosa","Versicolor","Virginica"]
+# iris_features = list(data.columns)
+# prediction_feature = "variety"
+# iris_features.remove(prediction_feature)
+# iris_dataset = TabularClassification(dataset_name="iris",X=data[iris_features],Y=data[prediction_feature],feature_names=iris_features,class_names=iris_class_names)
+# iris_timer = iris_dataset.get_explanations()
+
+
+#reddit_dataset
+# data = pd.read_csv("./datasets/reddit.csv")
+# reddit_type_dict = {"Topic" : {"Physics" : 0, "Chemistry" : 1,"Biology" : 2}}
+# data = data.replace(reddit_type_dict)
+# reddit_class_names = ["Physics","Chemistry","Biology"]
+# X = data['Comment'].values
+# Y = data['Topic'].values
+# reddit_dataset = TextClassification(dataset_name="Reddit",X=X,Y=Y,class_names=reddit_class_names)
+# reddit_dataset.get_explanations()
